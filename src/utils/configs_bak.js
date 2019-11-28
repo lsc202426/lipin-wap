366966000cd0f05b8c0abd37e470f364ce8592eb
linshichao <563951731@qq.com>
2019-11-28 14:29:05 +0800

    协议头修改--chao

--- src/utils/configs_bak.js
+++ src/utils/configs_bak.js
@@ -4,12 +4,12 @@ export default {
     // 客服电话
     link_phone: '400-628-1118',
     // 本地连接http://api.lipin.com
-    public_domain: process.env.NODE_ENV === 'development' ? 'http://10.100.5.126:88' : 'http://lipin-api.itmnic.com',
+    public_domain: process.env.NODE_ENV === 'development' ? 'http://10.100.5.126:88' : 'https://lipin-api.itmnic.com',
     // 线上连接
-    public_domain_line: 'http://lipin-api.itmnic.com',
+    public_domain_line: 'https://lipin-api.itmnic.com',
     // 中文地址
-    public_chinese_url: 'http://礼品.商标',
+    public_chinese_url: 'https://礼品.商标',
     // 英文地址
-    public_english_url: 'http://lipin.itmnic.com'
+    public_english_url: 'https://lipin.itmnic.com'
   }
 }
